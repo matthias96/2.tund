c2d017d563674450f0c185d8bb58144808cfdd33
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2016-02-10 10:49:31 +0200

    Moosipurk

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-ss
+  (function(){
+    "use strict";
+
+    var Moosipurk = function(){
+      // SINGELTON PATTERN(4 rida)
+      if(Moosipurk.instance){
+        return Moosipurk.instance;
+      }
+      Moosipurk.instance = this; // this viitab moosipurgile
+      console.log('moosipurgi sees');
+    };
+    window.onload=function (){
+      var app= new Moosipurk();
+      var app2= new Moosipurk();
+    };
+   })();
